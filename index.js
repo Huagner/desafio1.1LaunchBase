4f39292ea05ee28229c6ecd90b5da7bc03dacc8a
Huagner <huagnerxavier11@gmail.com>
2020-08-17 19:13:33 -0300

    importacao inicial completa

---
+++ index.js
@@ -0,0 +1,21 @@
+// Huagner Xavier
+// Aluno da Rocketseat, modalidade
+
+console.log('===========calulo de IMC =================')
+
+// declaracao de variaveis 
+let name = "Carlos";
+let peso = 84;
+let altura = 1.88;
+
+// calculo do IMC
+let IMC = peso/(altura*altura)
+// estrutura de consdicional
+ if (IMC >= 30) {
+     console.log(`${name}, você está acima do peso.`)
+ }// se maior ou igual que 30
+ else {
+     console.log(`${name}, você não está acima do peso.`)
+ }// se menor que 30
+
+
